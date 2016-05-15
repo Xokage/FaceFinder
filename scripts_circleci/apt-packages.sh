5696d4966f6d2e54289fe65cfb81f56870b8566a
Xoán Antelo Castro <monotrescabezas@gmail.com>
2016-05-15 23:26:30 +0200

    Engado o servidor mysql que me faltaba para facer os tests de django

--- scripts_circleci/apt-packages.sh
+++ scripts_circleci/apt-packages.sh
@@ -4,8 +4,10 @@
 if [ ! -e ~/aptcache ]; then
     mkdir ~/aptcache
     sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
+    sudo apt-add-repository -y 'deb http://ppa.launchpad.net/ondrej/mysql-experimental/ubuntu precise main'
     sudo apt-get -o dir::cache::archives="/home/ubuntu/aptcache" update; 
-    sudo apt-get -o dir::cache::archives="/home/ubuntu/aptcache" install -y build-essential cmake cmake-curses-gui qt5-default libqt5svg5-dev qtcreator git autoconf automake build-essential libass-dev libfreetype6-dev libgpac-dev libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libx11-dev libxext-dev libxfixes-dev pkg-config texi2html zlib1g-dev yasm python-pip python-dev libpng-dev libeigen3-dev libgtk2.0-dev
+    sudo DEBIAN_FRONTEND=noninteractive apt-get -o dir::cache::archives="/home/ubuntu/aptcache" install -y mysql-server-5.6
+    sudo apt-get -o dir::cache::archives="/home/ubuntu/aptcache" install -y build-essential cmake cmake-curses-gui qt5-default libqt5svg5-dev qtcreator git autoconf automake build-essential libass-dev libfreetype6-dev libgpac-dev libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libx11-dev libxext-dev libxfixes-dev pkg-config texi2html zlib1g-dev yasm python-pip python-dev libpng-dev libeigen3-dev libgtk2.0-dev mysql-server-5.6
 else
     sudo dpkg -i ~/aptcache/*.deb 
 fi
--- scripts_circleci/run_django_tests.sh
+++ scripts_circleci/run_django_tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 cd Web
+mysql -u root -e "CREATE USER facefinder@localhost IDENTIFIED BY 'facefinder2045'"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'facefinder'@'localhost'"
 python manage.py test
